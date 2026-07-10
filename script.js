947a06d23144fc2de1e03560f1457870853b6830
gabriel <boagabriel63@gmail.com>
2026-07-10 02:44:31 -0300

    feat: ajustado código

--- script.js
+++ script.js
@@ -1,29 +1,43 @@
-const button = document.querySelector('button');
-const resultadoDiv = document.getElementById('resultado');
-const tipoSelect = document.getElementById('tipo');
+const button = document.querySelector("button");
+const resultadoDiv = document.getElementById("resultado");
+const tipoSelect = document.getElementById("tipo");
 
 function gerarNumerosAleatorios(quantidade, maximo) {
+
     let numeros = [];
+
     while (numeros.length < quantidade) {
+
         let numeroAleatorio = Math.floor(Math.random() * maximo) + 1;
+
         if (!numeros.includes(numeroAleatorio)) {
             numeros.push(numeroAleatorio);
         }
     }
+
     return numeros.sort((a, b) => a - b);
 }
 
-button.addEventListener('click', () => {
+button.addEventListener("click", () => {
+
     const jogoSelecionado = tipoSelect.value;
+
     let numerosGerados = [];
 
-    if (jogoSelecionado === 'Mega-Sena') {
+    if (jogoSelecionado === "Mega-Sena") {
+
         numerosGerados = gerarNumerosAleatorios(6, 60);
-    } else if (jogoSelecionado === 'Quina') {
+
+    } else if (jogoSelecionado === "Quina") {
+
         numerosGerados = gerarNumerosAleatorios(5, 80);
-    } else if (jogoSelecionado === 'Lotofácil') {
+
+    } else if (jogoSelecionado === "Lotofácil") {
+
         numerosGerados = gerarNumerosAleatorios(15, 25);
+
     }
 
-    resultadoDiv.innerHTML = numerosGerados.join(' - ');
+    resultadoDiv.textContent = numerosGerados.join(" - ");
+
 });
